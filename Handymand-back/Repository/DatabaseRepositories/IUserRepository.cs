db30a7bfaa9458460239e4185192426bcc9a6c78
Bintescu Robert <bintescurobert@yahoo.com>
2022-01-24 22:13:21 +0200

    Bug Fixing

---
+++ Handymand-back/Repository/DatabaseRepositories/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Handymand.Models;
+using Handymand.Repository.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Handymand.Repository.DatabaseRepositories
+{
+    public interface IUserRepository: IGenericRepository<User>
+    {
+        User GetById(int Id);
+
+        User GetByIdIncludingAll(int Id);
+
+        List<User> GetAllWithInclude();
+
+        User GetByEmail(string email);
+    }
+}
---
+++ Handymand-back/Repository/DatabaseRepositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using Handymand.Data;
+using Handymand.Models;
+using Handymand.Repository.GenericRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Handymand.Repository.DatabaseRepositories
+{
+    public class UserRepository: GenericRepository<User>, IUserRepository
+    {
+        public UserRepository (HandymandContext context) : base(context)
+        {
+
+        }
+
+        public List<User> GetAllWithInclude()
+        {
+            return _table.Include(x => x.FreelancerAccount)
+                .Include(x => x.ClientAccount)
+                .ToList();
+        }
+
+        public User GetById(int Id)
+        {
+            return _table.Where(i => i.Id == Id).FirstOrDefault();
+        }
+
+        public User GetByIdIncludingAll(int Id)
+        {
+            return _table.Where(i => i.Id == Id)
+                .Include(x => x.ClientAccount)
+                .Include(x => x.FreelancerAccount).FirstOrDefault();
+
+        }
+
+        public User GetByEmail(string email)
+        {
+            return _table.Where(i => String.Equals(i.Email, email)).FirstOrDefault();
+        }
+    }
+}
